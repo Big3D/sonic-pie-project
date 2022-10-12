c21c4aff00368752e96e0c0ffc531e2de0b7fa73
Dustin Daves <biginitiald@gmail.com>
2022-10-12 11:40:47 -0700

    Rebased-Code

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
 //adds click event listener to the start button
 document.getElementById("start-button").addEventListener("click", startGame);
-
-//function that will start the canvas game
-// 
-function startGame() {
-  console.log("start");
-}
-
-//Menu Canvas Design
 const Menu_Canvas = document.getElementById("menu_canvas");
 let mtx = Menu_Canvas.getContext("2d");
-
 let start_button = document.getElementById("start-button");
 let leaderboard_button = document.getElementById("leaderboard-button");
 leaderBoardMenu.style.display = "none";
@@ -71,10 +62,6 @@ start_button.addEventListener("click", function () {
   let score = 0;
 
   // player class
-
-  const playerSprite1 = new Image();
-  playerSprite1.src = "/img/TEST-Catwalk copy.png";
-
   class Player {
     constructor() {
       this.position = {
@@ -86,271 +73,127 @@ start_button.addEventListener("click", function () {
         y: 0,
       };
       // size of player
-      this.width = 160;
-      this.height = 120;
-      this.image = playerSprite1;
+      this.width = 30;
+      this.height = 30;
     }
 
     // render player
     draw() {
-      // ctx.fillStyle = "blue";
-      // ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-      mtx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
-    }q
-
-//logo square
-mtx.beginPath();
-mtx.lineWidth = "2";
-mtx.rect(100, 20, 500, 100);
-mtx.stroke();
-
-//start button
-mtx.beginPath();
-mtx.lineWidth = "2";
-
-mtx.rect(100, 400, 50, 25);
-mtx.stroke();
-
-//leaderboard button
-mtx.beginPath();
-mtx.lineWidth = "2";
-mtx.rect(516, 400, 84, 25);
-mtx.stroke();
-
-// const MainLogo_Canvas = document.getElementById("main_logo");
-// let mLtx = Menu_Canvas.getContext("2d");
-// mLtx.moveTo(0, 0);
-// mLtx.lineTo(0, 0);
-// mLtx.stroke();
-
-// // Btn Canvas Design
-// const Btn_Canvas = document.getElementById("btn_play");
-// let btx = Btn_Canvas.getContext("2d");
-// btx.beginPath();
-// btx.arc(5, 5, 40, 0, 2 * Math.PI);
-// btx.stroke()
-
-// Character movement
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-
-// gravity
-const gravity = 0.5;
-//SCORE
-let score = 0;
-
-// player class
-class Player {
-  constructor() {
-    this.position = {
-      x: 10,
-      y: 0
-    };
-    this.velocity = {
-      x: 0,
-      y: 10,
-    };
-    // size of player
-    this.width = 30;
-    this.height = 30;
-  }
-
-
-  // render player
-  draw() {
-    ctx.fillStyle = "blue";
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-
-  update() {
-    this.draw();
-    this.position.y += this.velocity.y;
-    this.position.x += this.velocity.x;
-
-    // makes player position always hit the ground of the canvas
-    if (this.position.y + this.height + this.velocity.y <= canvas.height)
-      this.velocity.y += gravity;
-    else {
-      this.velocity.y = 0;
-    }
-  }
- 
-}
-
-// obstacle class
-class Obstacle {
-  constructor({ position, velocity, distance }) {
-    this.position = {
-      x: position.x,
-      y: position.y,
-    };
-    this.velocity = {
-      x: velocity.x,
-      y: velocity.y,
-    };
-    this.width = 30;
-    this.height = 30;
-
-    this.distance = distance;
-  }
-
-  draw() {
-    ctx.fillStyle = "red";
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-
-  update() {
-    this.draw();
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-
-    // sets obstacle "y" position to bottom of canvas
-    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
-      this.velocity.y += gravity;
-    } else {
-      this.velocity.y = 0;
+      ctx.fillStyle = "blue";
+      ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 
-    // makes obstacle go back and forth
-    this.distance.traveled += Math.abs(this.velocity.x);
+    update() {
+      this.draw();
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
 
-    if (this.distance.traveled > this.distance.limit) {
-      this.distance.traveled = 0;
-      this.velocity.x = -this.velocity.x;
+      // makes player position always hit the ground of the canvas
+      if (this.position.y + this.height + this.velocity.y <= canvas.height)
+        this.velocity.y += gravity;
+      else {
+        this.velocity.y = 0;
+      }
     }
   }
 
-}
+  // obstacle class
+  class Obstacle {
+    constructor({ position, velocity, distance }) {
+      this.position = {
+        x: position.x,
+        y: position.y,
+      };
+      this.velocity = {
+        x: velocity.x,
+        y: velocity.y,
+      };
+      this.width = 30;
+      this.height = 30;
 
-// CLASS CONTRUCTOR FOR PLATFORMS
-class Platform {
-  constructor({x,y}){
-    this.position = {
-      x,
-      y
-
+      this.distance = distance;
     }
-    this.width = 200
-    this.height = 35
+
+    draw() {
+      ctx.fillStyle = "red";
+      ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update() {
+      this.draw();
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+
+      // sets obstacle "y" position to bottom of canvas
+      if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+        this.velocity.y += gravity;
+      } else {
+        this.velocity.y = 0;
+      }
+
+      // makes obstacle go back and forth
+      this.distance.traveled += Math.abs(this.velocity.x);
+
+      if (this.distance.traveled > this.distance.limit) {
+        this.distance.traveled = 0;
+        this.velocity.x = -this.velocity.x;
+      }
+    }
   }
-  draw(){
-    // ctx.fillStyle = 'red'
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
-  }
-}
 
+  // new instance - sonic
+  const sonic = new Player();
 
-// new instance - sonic
-const sonic = new Player();
-// new instance for platforms
-const platforms = [new Platform({x:1000,y:850}), new Platform({x:300,y:800})]
+  // health bar
+  let health = 100;
 
+  // new moving obstacles
+  let obstacles = [];
 
-// health bar
-let health = 100;
+  obstacles = [
+    new Obstacle({
+      position: {
+        x: 400,
+        y: 400,
+      },
+      velocity: {
+        x: -0.5,
+        y: 0,
+      },
+      distance: {
+        limit: 100,
+        traveled: 0,
+      },
+    }),
+    new Obstacle({
+      position: {
+        x: 800,
+        y: 400,
+      },
+      velocity: {
+        x: -0.5,
+        y: 0,
+      },
+      distance: {
+        limit: 100,
+        traveled: 0,
+      },
+    }),
+  ];
 
-// new moving obstacles
-let obstacles = [];
-
-obstacles = [
-  new Obstacle({
-    position: {
-      x: 400,
-      y: 400,
+  const keys = {
+    right: {
+      pressed: false,
     },
-    velocity: {
-      x: -0.5,
-      y: 0,
+    left: {
+      pressed: false,
     },
-    distance: {
-      limit: 100,
-      traveled: 0,
-    },
-  }),
+  };
 
-  new Obstacle({
-    position: {
-      x: 800,
-      y: 400,
-    },
-    velocity: {
-      x: -0.5,
-      y: 0,
-    },
-    distance: {
-      limit: 100,
-      traveled: 0,
-    },
-  }),
-];
-
-const keys = {
-  right: {
-    pressed: false,
-  },
-  left: {
-    pressed: false,
-  },
-  spacebar: {
-    pressed: false,
-  },
-};
-
-
-// RequestAnimation Frame and set control
-//let requestAnimationFrame = window.requestAnimationFrame;
-let keepAnimating = true;
-function animate() {
-  requestAnimationFrame(animate);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  sonic.update();
-//loops through platforms arrray
-  platforms.forEach((platform)=>{
-    platform.draw()
-  })
-  // platform.draw()
-
-  if (keys.right.pressed) {
-    sonic.velocity.x = 5;
-    // SHOW SCORE 
-    score = (sonic.position.x/2)
-    if(score === 100){
-      document.getElementById('currentScore').innerHTML = `Score: ${score}`
-    }
-    if(score === 300){
-      document.getElementById('currentScore').innerHTML = `Score: ${score}`
-    }
-    if(score === 500){
-      document.getElementById('currentScore').innerHTML = `Score: ${score}`
-    }
-  } else if (keys.left.pressed) {
-    sonic.velocity.x = -5;
-  } else 
-    sonic.velocity.x = 0;
-  
-//platform collision 
-platforms.forEach((platform)=>{
-  if(sonic.position.y + sonic.height <= platform.position.y
-    && sonic.position.y + sonic.height + sonic.velocity.y >= platform.position.y
-    && sonic.position.x + sonic.width >= platform.position.x 
-    && sonic.position.x <= platform.position.x + platform.width
-    ){
-      sonic.velocity.y = 0
-    }
-  })
-
-
-    // This stops the function if keepAnimating is false
-  if(!keepAnimating){
-    return;
-  }
-  
-  else {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  function animate() {
     requestAnimationFrame(animate);
-  
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     // updates each obstacle in the array
     obstacles.forEach((obstacle) => {
       // detects for collision between obstacle and player
@@ -369,12 +212,12 @@ platforms.forEach((platform)=>{
           sonic.position.y -= 50;
           sonic.position.x -= 150;
         }
-        // **for testing purposes only**
+        // for testing purposes only
         console.log(health);
       }
       obstacle.update();
     });
-  
+
     // updates player
     sonic.update();
 
@@ -390,10 +233,6 @@ platforms.forEach((platform)=>{
       sonic.velocity.x = 0;
     }
   }
-}
-  
-
-
 
   animate();
 
@@ -428,24 +267,21 @@ platforms.forEach((platform)=>{
   });
 
   // Timer
-  let i = 60;
-  function onTimer() {
-
-    console.log("timer  running");
-
-    document.getElementById("countdown").innerHTML = i;
-    i--;
-    if (i < 0) {
-      clearInterval(i);
-      if (i === 0) {
-        alert("Game Over!");
-      }
-    } else {
-      setTimeout(onTimer, 1000);
-    }
-  }
 });
 
+i = 60;
+function onTimer() {
+  document.getElementById("countdown").innerHTML = i;
+  i--;
+  if (i < 0) {
+    clearInterval(i);
+    if (i === 0) {
+      alert("Game Over!");
+    }
+  } else {
+    setTimeout(onTimer, 1000);
+  }
+}
 //function that will start the canvas game
 function startGame() {
   console.log("start");
@@ -465,6 +301,7 @@ class TitleBackground {
     this.image = titleImage;
     this.width = 1920;
     this.height = 1080;
+    console.log(this.image);
   }
 
   draw() {
@@ -476,18 +313,12 @@ class TitleLogo {
     this.position = { x, y };
 
     this.image = logoImage;
-    this.width = 400;
-    this.height = 400;
+    this.width = 200;
+    this.height = 200;
   }
 
   draw() {
-    mtx.drawImage(
-      this.image,
-      this.position.x,
-      this.position.y,
-      this.width,
-      this.height
-    );
+    mtx.drawImage(this.image, this.position.x, this.position.y);
   }
 }
 
@@ -497,7 +328,7 @@ const titleScreen = new TitleBackground({ x: 0, y: -10, titleImage });
 let logoImage = new Image();
 logoImage = titleLogo;
 const titleLogoBig = new TitleLogo({
-  x: Menu_Canvas.width / 2 - logoImage.width / 2,
+  x: Menu_Canvas.width / 2 - logoImage.width / 2.1,
   y: 10,
   logoImage,
 });
@@ -509,154 +340,3 @@ titleImage.onload = function () {
 logoImage.onload = function () {
   titleLogoBig.draw();
 };
-
-animate();
-// character movement on keydown
-addEventListener("keydown", ({ keyCode }) => {
-  switch (keyCode) {
-    case 37:
-      keys.left.pressed = true;
-      break;
-    case 39:
-      keys.right.pressed = true;
-      break;
-    case 32:
-      if(!keys.spacebar.pressed){
-        keys.spacebar.pressed = true
-        sonic.velocity.y -= 15;
-      }
-     
-      break;
-  }
-});
-
-// character movement on key up
-addEventListener("keyup", ({ keyCode }) => {
-  switch (keyCode) {
-    case 37:
-      keys.left.pressed = false;
-      break;
-    case 39:
-      keys.right.pressed = false;
-      break;
-    case 32:
-      // sonic.velocity.y -= 10;
-      keys.spacebar.pressed = false
-      if(!keys.spacebar.pressed){
-        sonic.velocity.y += 15
-      
-      }
-      break;
-  }
-});
-
-// Timer 
-        i = 60;
-        function onTimer() {
-            
-            document.getElementById("countdown").innerHTML = i;
-            i--;
-            if (i < 0) {
-                clearInterval(i);
-                if(i === 0) {
-                    alert("Game Over!");   
-                    }
-              }
-            else {
-                setTimeout(onTimer, 1000);
-            }
-        }
-
-// Timer (Refactored a bit to ensure proper behaviour of the pause function) T.A
-let i = 60; 
-let timeout; 
-let stopTime = 0;
-
-function onTimer() {
-  document.getElementById("countdown").innerHTML = i;
-  i--;
-  timeout = setTimeout(onTimer, 1000);
-  if (i <= stopTime) {
-    clearTimeout(timeout);
-    location.reload()
-  } 
-}
-
-//Paws Menu
-//Open press the ESC it should have resume and quit buttons.
-const paws_Menu = document.querySelector("#paws");
-
-addEventListener("keydown", (e) => {
-  let name = e.key;
-  let code = e.code;
-
-  if (code === "Escape" && name === "Escape") {
-    displayPaws();
-    clearTimer()
-    keepAnimating = false;
-  }
-  else {
-    Resume()
-  }
-});
-
-
-paws_Menu.style.display = "none";
-function displayPaws() {
-  if (paws_Menu.style.display === "none") {
-    paws_Menu.style.display = "flex";
-  } else {
-    paws_Menu.style.display = "none";
-  }
-
-}
-
-// Clear Timer 
-//get the current timer 
-current_timer = document.getElementById("countdown").innerHTML
-// stop the current on the click of esacpe key
- function clearTimer(){
-  
-  clearTimeout(timeout)
-
- }
-
-  // Quit Button
-  const quit_btn = document.querySelector("#btnQ")
-  quit_btn.addEventListener("click", Quit)
-  function Quit() {
-    location.reload()
-    paws_Menu.style.display = "none";
-    timeout = setTimeout(onTimer, 1000)
-    return current_timer = timeout
-  };
-  
-  // Resume Button
-  /**
-   * While paused gameplay should also pause
-    -Timer stops
-    -Player cannot move
-    -Obstacles should stop
-   */
-
-   
-
-  const resume_btn = document.querySelector("#btnS")
-  resume_btn.addEventListener("click", Resume)
-  function Resume () {
-   onTimer()
-   paws_Menu.style.display = "none";
-   requestAnimationFrame(animate);
-   keepAnimating = true;
-  };
-
-   // This that can be improved on(getting escape to pause and play)
-
-  /**
-   * Things we need to fix asap 
-   * Ontimer function 
-   * The right key press speeds up the timer;
-   * Popping up leaderboard
-   * 
-   */
-
