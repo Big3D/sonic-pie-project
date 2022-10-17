59f41b4a2b9a667390870af0fea6cfe4302a0566
Kendall Fuller <kendallfuller97@yahoo.com>
2022-10-17 10:30:07 -0700

    docs(Code Added & Re-added Unmerged Code)

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -30,3 +30,40 @@ let x = document.getElementById("leaderBoardMenu");
     x.style.display = "none";
 }
 }
+
+
+// Cyclic Input JS
+KEYCODES = { left: 37, up: 38, right: 39, down: 40 };
+
+$('.cyclic_input').on('keydown',function(ev){
+    input = $(this);
+    val = $(this).text();
+    
+    switch (ev.keyCode) {   
+      case KEYCODES.right:
+        input.next().focus();
+        break;
+      case KEYCODES.left:
+        input.prev().focus();
+        break;
+      case KEYCODES.up:
+        input.text(advanceCharBy(val, 1));
+        break;
+      case KEYCODES.down:
+        input.text(advanceCharBy(val, -1));
+        break;
+      default:
+        if (ev.keyCode >= 65 && ev.keyCode <= 65 + 26) {
+            input.text(String.fromCharCode(ev.keyCode));
+            input.next().focus()
+        }
+    };
+    ev.preventDefault();
+});
+
+advanceCharBy = function(char, distance) {
+    oldCode = char.charCodeAt(0);
+    newCode = 65 + (oldCode - 65 + 26 + distance) % 26;
+    return String.fromCharCode(newCode);
+};
+// End of Cyclic Input JS
